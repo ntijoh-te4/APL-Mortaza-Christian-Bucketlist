b1863a31e3e560b567ba54bce109260a571e4861
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2024-01-15 14:55:33 +0100

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ frontend/App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <h1>Bucketlist: Din lösning för Todos</h1>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
