e825076af382b175ae78243d6c82c603e1dcb440
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2024-01-16 10:39:45 +0100

    removed key "deadline"

--- frontend/components/ItemList.tsx
+++ frontend/components/ItemList.tsx
@@ -3,11 +3,10 @@ import styles from "../styles/ItemList.module.css";
 import { useState } from "react";
 
 const ItemList = () => {
-
   const [fakeItems, setItems] = useState([
-    { id: 1, description: "Drive Jetski", deadline: "28th February" },
-    { id: 2, description: "Skydive", deadline: "51st June" },
-    { id: 3, description: "Become president", deadline: "23rd December" },
+    { id: 1, description: "Drive Jetski" },
+    { id: 2, description: "Skydive" },
+    { id: 3, description: "Become president" },
   ]);
 
   return (
@@ -15,7 +14,7 @@ const ItemList = () => {
       <h1>Items List</h1>
       <ul style={styles.ul}>
         {fakeItems.map((fakeItem) => (
-          <Item description={fakeItem.description} key={fakeItem.id}/>
+          <Item description={fakeItem.description} key={fakeItem.id} />
         ))}
       </ul>
     </div>
