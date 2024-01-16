33db05193fc175070f7d70be0acfd56973c31a59
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2024-01-16 10:55:00 +0100

    Items are now passed as a prop

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchBar from './components/SearchBar';
 import AddForm from './components/AddForm';
 import ItemList from './components/ItemList';
 
 export default function App() {
+
+  const [fakeItems, setItems] = useState([
+    { id: 1, description: "Drive Jetski" },
+    { id: 2, description: "Skydive" },
+    { id: 3, description: "Become president" },
+  ]);
+
+  function deleteItem(id: number): void {
+    console.log('delete', id)
+  }
+
+
   return (
     <View style={styles.container}>
       <SearchBar/>
       <AddForm/>
-      <ItemList/>
+      <ItemList fakeItems={fakeItems} onDelete={deleteItem}/>
     </View>
   );
 }
--- frontend/components/ItemList.tsx
+++ frontend/components/ItemList.tsx
@@ -1,20 +1,14 @@
 import Item from "./Item";
 import styles from "../styles/ItemList.module.css";
-import { useState } from "react";
 
-const ItemList = () => {
-  const [fakeItems, setItems] = useState([
-    { id: 1, description: "Drive Jetski" },
-    { id: 2, description: "Skydive" },
-    { id: 3, description: "Become president" },
-  ]);
+const ItemList = ({ fakeItems, onDelete }) => {
 
   return (
     <div>
       <h1>Items List</h1>
       <ul style={styles.ul}>
         {fakeItems.map((fakeItem) => (
-          <Item description={fakeItem.description} key={fakeItem.id} />
+          <Item key={fakeItem.id} description={fakeItem.description} onDelete={onDelete} />
         ))}
       </ul>
     </div>
