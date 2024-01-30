f15ca89441fa204718e5fdb697d907b603892d99
ntijoh-mortaza-ebeid <mortaza.ebeid@elev.ga.ntig.se>
2024-01-30 13:33:59 +0100

    added migrations

---
+++ backend/Migrations/20240130123309_UpdatedTodoListAndItem.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Bucketlist.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTodoListAndItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "TodoItems",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Deadline",
+                table: "TodoItems",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "TodoItems",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "TodoListId",
+                table: "TodoItems",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "TodoItems",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "TodoLists",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_TodoListId",
+                table: "TodoItems",
+                column: "TodoListId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_TodoLists_TodoListId",
+                table: "TodoItems",
+                column: "TodoListId",
+                principalTable: "TodoLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_TodoLists_TodoListId",
+                table: "TodoItems");
+
+            migrationBuilder.DropTable(
+                name: "TodoLists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TodoItems_TodoListId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "TodoListId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "TodoItems");
+        }
+    }
+}
