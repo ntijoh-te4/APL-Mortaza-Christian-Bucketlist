047038a899c98201947ac80fa5e69f85b92509f8
ntijoh-mortaza-ebeid <mortaza.ebeid@elev.ga.ntig.se>
2024-01-30 13:30:42 +0100

    made Deadline field in TodoItem model nullable for when no deadline is set

--- backend/Models/TodoItem.cs
+++ backend/Models/TodoItem.cs
@@ -6,7 +6,7 @@ public class TodoItem
     public string Title { get; set; } = "";
     public string Description { get; set; } = "";
     public bool IsComplete { get; set; } = false;
-    public DateTime Deadline { get; set; }
+    public DateTime? Deadline { get; set; } = null;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public TodoList TodoList { get; set;} = null!;
