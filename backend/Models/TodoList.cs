34ad6e6eb07abff49d673b17e2ee7ea971a86ed7
ntijoh-mortaza-ebeid <mortaza.ebeid@elev.ga.ntig.se>
2024-01-30 13:48:33 +0100

    added timestamps for TodoList model

--- backend/Models/TodoList.cs
+++ backend/Models/TodoList.cs
@@ -4,5 +4,7 @@ public class TodoList
 {
     public long Id { get; set; }
     public string Name { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
     public List<TodoItem> TodoItems { get; set; } = [];
 }
