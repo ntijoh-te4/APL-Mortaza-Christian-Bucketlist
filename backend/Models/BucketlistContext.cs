d7df2e8b27138780cd1c294703012c3f9c0f087e
ntijoh-mortaza-ebeid <mortaza.ebeid@elev.ga.ntig.se>
2024-01-30 13:32:07 +0100

    added TodoList model to DbContext

--- backend/Models/BucketlistContext.cs
+++ backend/Models/BucketlistContext.cs
@@ -5,6 +5,7 @@ namespace Bucketlist.Models;
 public class BucketlistContext(DbContextOptions<BucketlistContext> options) : DbContext(options)
 {
     public DbSet<TodoItem> TodoItems { get; set; } = default!;
+    public DbSet<TodoList> TodoLists { get; set; } = default!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
