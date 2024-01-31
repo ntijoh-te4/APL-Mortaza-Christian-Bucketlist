6452fc4ff639ee47ac1f6a941b6b3f007d6213e2
ntijoh-mortaza-ebeid <mortaza.ebeid@elev.ga.ntig.se>
2024-01-31 16:18:59 +0100

    updated seeding to match new database

--- backend/DatabaseInitializer.cs
+++ backend/DatabaseInitializer.cs
@@ -1,4 +1,5 @@
 using Bucketlist.Models;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace Bucketlist.DatabaseInitializer;
@@ -41,14 +42,14 @@ public class DatabaseInitializer
     private static void SeedData(BucketlistContext context)
     {
         TodoList[] todoLists = [
-            new TodoList { Name = "todolist 1"},
-            new TodoList { Name = "todolist 2"}
+            new TodoList { Name = "todolist 1" },
+            new TodoList { Name = "todolist 2" }
         ];
-        
+
         context.TodoLists.AddRange(todoLists);
 
         TodoItem[] todoItems = [
-            new TodoItem { Title = "Become the president", TodoList = todoLists[0] },
+            new TodoItem { Title = "Become the president", TodoList = todoLists[0], Deadline = DateTime.UtcNow.AddDays(7) },
             new TodoItem { Description = "Read Moby Dick", TodoList = todoLists[1] },
             new TodoItem { Description = "Create more todo items", IsComplete = true, TodoList = todoLists[0] }
         ];
