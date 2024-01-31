d1f571d7b13b5090dd48999371f157681aa2f3cc
ntijoh-mortaza-ebeid <mortaza.ebeid@elev.ga.ntig.se>
2024-01-31 13:56:40 +0100

    changed Deadline field in TodoItem model to be non-nullable and having default of DateTime.MinValue or -infinity in database

---
+++ backend/Migrations/20240131125230_DeadlineNullToMinValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bucketlist.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeadlineNullToMinValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "deadline",
+                table: "todo_items",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "deadline",
+                table: "todo_items",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
--- backend/Models/TodoItem.cs
+++ backend/Models/TodoItem.cs
@@ -6,6 +6,6 @@ public class TodoItem : BaseEntity
     public string Title { get; set; } = "";
     public string Description { get; set; } = "";
     public bool IsComplete { get; set; } = false;
-    public DateTime? Deadline { get; set; } = null;
+    public DateTime Deadline { get; set; } = DateTime.MinValue;
     public TodoList TodoList { get; set; } = null!;
 }
