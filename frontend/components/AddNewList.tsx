ab6dea9eb2bd1cace1fc8f133320dff83a46e2a1
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2024-02-05 14:56:39 +0100

    Added submission handling for todoLists

--- frontend/components/AddNewList.tsx
+++ frontend/components/AddNewList.tsx
@@ -1,12 +1,26 @@
 import { FC, useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 const AddNewList: FC = () => {
   const [isPressed, setIsPressed] = useState(false);
+  const [formIsActive, setFormIsActive] = useState(false);
+  const [listName, setListName] = useState("");
+
+  function toggleFormStatus() {
+    setFormIsActive(!formIsActive);
+  }
+
+  function handleSubmit() {
+    console.log("List name submitted:", listName);
+    setListName("");
+    setFormIsActive(false);
+  }
 
   return (
-    <View style={{ zIndex: -1 }}>
+    <View style={{ zIndex: -1, display: "flex", flexDirection: "row" }}>
       <Pressable
+        onPress={toggleFormStatus}
         onPressIn={() => setIsPressed(true)}
         onPressOut={() => setIsPressed(false)}
         style={[
@@ -20,6 +34,19 @@ const AddNewList: FC = () => {
       >
         <Text style={styles.h1}>+ Add new list</Text>
       </Pressable>
+      {formIsActive ? (
+        <View style={styles.listTemplate}>
+          <TextInput
+            placeholder="Enter List Name"
+            value={listName}
+            onChangeText={(text) => setListName(text)}
+            onSubmitEditing={handleSubmit}
+          />
+          <Pressable onPress={handleSubmit}>
+            <Text>Submit</Text>
+          </Pressable>
+        </View>
+      ) : null}
     </View>
   );
 };
@@ -29,6 +56,16 @@ const styles = StyleSheet.create({
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    width: 300,
+    height: 500,
+    borderRadius: 12,
+    marginHorizontal: 16,
+  },
+  listTemplate: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "gold",
     width: 300,
     height: 500,
     borderRadius: 12,
