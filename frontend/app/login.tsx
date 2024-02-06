d218b1611957cab34c66f362729e29932cfc70d5
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2024-02-06 15:52:09 +0100

    login skeleton

---
+++ frontend/app/login.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="text" placeholder="Password" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
