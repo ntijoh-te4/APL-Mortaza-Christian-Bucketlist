2135855753ab708a5a68d42c16ea86db06311451
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2024-02-07 14:11:29 +0100

    getter and setter for id

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,7 +1,7 @@
 namespace Bucketlist.Models;
 public class User
 {
-    public long Id
+    public long Id { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
 }
