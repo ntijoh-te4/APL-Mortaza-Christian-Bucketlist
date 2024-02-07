8057ff90743480a9c398d8621c90a9e463ff163c
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2024-02-07 14:22:37 +0100

    added default values to put warning aside

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -2,6 +2,6 @@ namespace Bucketlist.Models;
 public class User
 {
     public long Id { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    public string UserName { get; set; } = "";
+    public string Password { get; set; } = "";
 }
